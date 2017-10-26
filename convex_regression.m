21fc4f467919b56829587bb3131b43b4e48e8c63
Mihaela Curmei <curmeimihaela@gmail.com>
2017-10-26 11:16:39 -0700

    updating convex regression

--- convex_regression.m
+++ convex_regression.m
@@ -37,15 +37,36 @@ x=sdpvar(1,k);
 % currently computing the objective is the biggest computational bottleneck
 % I tried to vectorized versions, but they lead to errors
 
-monom_bulk = bulkeval(v,x,features');
-peval_bulk = c'*monom_bulk;
-diff_bulk = peval_bulk - response';
-h = diff_bulk*diff_bulk';
+monom_bulk = bulkeval(v,x,features'); % <- evaluates all monomials for all values of the features
+% >> monom_bulk
+% monom_bulk =
+%   Columns 1 through 12
+%     1.0000    1.0000    1.0000    1.0000    1.0000    1.0000    1.0000    1.0000    1.0000    1.0000    1.0000    1.0000
+%     1.2557    1.2344    1.8156    1.0297    1.1742    1.9453    0.5634    1.9594    0.7838    1.5007    1.3797    1.5127
+%     1.1101    1.5004    1.9006    1.7164    ... 
+
+peval_bulk = c'*monom_bulk;% <- computes the value of the polynomial given the value of
+                           %    the argument from future, as a function of
+                           %    polynomial coefficients c
+% >> display(peval_bulk)
+%   Linear matrix variable 1x100 (full, real, 210 variables)
+% >> sdisplay(peval_bulk(1,1))
+%   c(1)+1.25567117866*c(2)+1.11009037263*c(3)+1.25576012277*c(4)+0.710383038904*c(5)+...
+
+diff_bulk = peval_bulk - response'; % <- computes the difference between
+                                    %    the function value at the feature
+                                    %    input and the response, (as a
+                                    %    function of c)
+h = diff_bulk*diff_bulk'; % <- h is the minimization objective, the sum of 
+                          %    squared errors
 
 %% PROBLEM SETUP: Define the decision variables used in the constraints
 
-% Create the monomials used in the constraints
-monomials = monolist(x, degree-2);
+% Create helper free variable
+y=sdpvar(1,k);
+
+% Create the monomials of the helper polynomials used in the constraints
+monomials = monolist([x y], degree-2);
 
 % Define the coefficients of the array of helper polynomials
 coef_help = sdpvar(k, length(monomials));
@@ -53,15 +74,13 @@ coef_help = sdpvar(k, length(monomials));
 % Create an array of helper polynomials 
 Q_help = coef_help*monomials;
 
-Q_inf = reshape(Q_inf, k, k, []);
-Q_sup = reshape(Q_sup, k, k, []);
-
 %% PROBLEM SETUP: Write the constraints
-F = [sos(Q_inf), sos(Q_sup)];
-F = F+[sos(transpose(jacobian(p,x)).*monotone_profile - Q_inf*transpose(x-inf_bound) - Q_sup*transpose(sup_bound-x))];
+F = [sos(Q_help)];
+F = F+[sos(y*hessian(p,x)*transpose(y).*convex_sign-(x-inf_bound).*(sup_bound-x)*Q_help)];
 
 %% SOS OPTIMIZATION: Fit the desired polynomial
 options = sdpsettings('verbose',2, 'solver', 'mosek');
-all_coef = [c;reshape(coef_sup, k*k*length(monomials),1,[]); reshape(coef_inf, k*k*length(monomials),1,[])];
+% The coefficients are the decision variables, putting them all in an array
+all_coef = [c;reshape(coef_help, k*length(monomials),1,[])];
 [sol,m,B,residual]=solvesos(F, h, options, all_coef);
 end

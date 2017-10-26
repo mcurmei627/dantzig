d1ed31e47570499195bc5b998745a67ab55cf26e
Mihaela Curmei <curmeimihaela@gmail.com>
2017-10-26 14:03:37 -0700

    Adding unconstrined polynomial regression function

---
+++ unconstrained_regression.m
@@ -0,0 +1,21 @@
+function mdl = unconstrained_regression(degree,features,response)
+%% Unconstrained polynomial regression
+% Outputs
+%   mdl := model from fitlm customized for multivariate polynomial regression
+% Inputs
+%   degree := the degree  of the decision polynomial 
+%   features := feature variable data used for training
+%   response := response variable used for training
+
+%%
+%k - number of features
+[~, k] = size(features); 
+
+% this is a trick to make fitlm fit a polynomial, for example if
+% mdl_spec is 'poly666' then fitml will fit a polynomial of degree 6 in the
+% first 3 features
+mdl_spec = strcat('poly', repmat(int2str(degree),1, k));
+
+%%
+mdl = fitlm(features,response',mdl_spec);
+end

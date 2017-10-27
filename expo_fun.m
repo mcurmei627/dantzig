876e594ef723d1e52e30f083f17dd09fe3cd1b37
Mihaela Curmei <curmeimihaela@gmail.com>
2017-10-27 11:25:40 -0700

    Updating and adding 'real' functions

--- expo_fun.m
+++ expo_fun.m
@@ -1,9 +1,10 @@
-function f = ladder_fun(arg)
+function f = expo_fun(arg)
 %% Description
 % Outputs
 %   f := a column vector of the evaluations of the function
 % Inputs
 %   arg := the input of the function 
-[~, k] = size(arg); 
-f=10./(1+1*exp((-1*sum(arg,2)+1.25*k)));
+[~, k] = size(arg);
+arg_af=(sum(arg,2)-0.5*k)/(1.5*k);
+f=(arg_af.*log(arg_af))*10+5;
 end

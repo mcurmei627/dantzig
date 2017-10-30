dead65ae15f902d9fc75eb839b17bd2b56137f82
Mihaela Curmei <curmeimihaela@gmail.com>
2017-10-30 14:34:28 -0700

    Adding function and example for the hybrid case (monotone and convex)

---
+++ hybrid_example.m
@@ -0,0 +1,8 @@
+% This script determines the mean and the sd of the testing error for the
+% weekly wage problem
+
+N_trials = 100;
+
+err_test_arr = zeros(100,1);
+err_test_u_arr = zeros(100,1);
+duration

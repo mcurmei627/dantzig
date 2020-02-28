7d211add1f6a3caa5a613faef23080d881ce8f97
Mihaela Curmei <curmeimihaela@gmail.com>
2020-02-27 18:33:19 -0800

    Adding KLEMS plots + hacky code

--- Experiments/KLEMS/usa_klems_experiment.m
+++ Experiments/KLEMS/usa_klems_experiment.m
@@ -2,7 +2,7 @@
 filename = fullfile('usa_wk_mar_2017.csv');
 production_data = csvread(filename,1,0); % offset by one row to skip the headers;
 industry_codes = readtable('industry_codes.csv');
-%%
+
 % loop through the industries
 N_industry = length(industry_codes.Industry);
 cd_rmse_train_arr = zeros(N_industry, 1);
@@ -10,6 +10,7 @@ cd_rmse_test_arr = zeros(N_industry, 1);
 mc_rmse_train_arr = zeros(N_industry, 1);
 mc_rmse_test_arr = zeros(N_industry, 1);
 
+%%
 for i=1:N_industry
     code = industry_codes.Industry(i);
     description = industry_codes.Description{i,1};
@@ -101,20 +102,125 @@ end
 
 %% Compare the algorithms
 
-% Compute competitive ratio
-comp_ratio = cd_rmse_test_arr./mc_rmse_test_arr;
-[comp_ratio,sortIdx] = sort(comp_ratio,'descend');
+%% RMSE comparison
+[mc_rmse_test_sorted,sortIdx] = sort(mc_rmse_test_arr);
+cd_rmse_test_sorted = cd_rmse_test_arr(sortIdx);
+
 % sort B using the sorting index
 labels = industry_codes.Description(sortIdx);
 % remove nans
-nanIdx = ~isnan(comp_ratio);
-comp_ratio = comp_ratio(nanIdx);
+nanIdx = ~isnan(cd_rmse_test_sorted) & ~isnan(mc_rmse_test_sorted);
+cd_rmse_test_sorted = cd_rmse_test_sorted(nanIdx);
+mc_rmse_test_sorted = mc_rmse_test_sorted(nanIdx);
+
+fig = figure();
+c_map = lines(3);
+hold on
+b1 = bar(mc_rmse_test_sorted,'stack');
+b1.FaceColor = c_map(1,:);
+b1.EdgeAlpha = 0;
+b2 = bar(cd_rmse_test_sorted,'stack');
+b2.EdgeColor = c_map(3,:);
+b2.FaceColor = c_map(3,:);
+b2.FaceAlpha = 0.3;
+b2.EdgeAlpha = 1;
+hold off
+
+ylabel('RMSE', 'fontsize', 14)
+
+
+lgd = legend('Monotone-Concave Polynomial', 'Cobb-Douglas');
+set(lgd, 'Location', 'Best')
+%set(gca,'xticklabel',labels,'fontsize',9)
+%xtickangle(50)
+set(gca,'yticklabel',num2str(abs(get(gca,'ytick').')))
+
+name = "comp_RMSE_test";
+fig_name = name + '.fig';
+png_name = name + '.png';
+savefig(fig_name);
+saveas(fig,png_name,'png');
+
+
+%% Compute competitive ratio
+comp_ratio_test = cd_rmse_test_arr./mc_rmse_test_arr;
+comp_ratio_train = cd_rmse_train_arr./mc_rmse_train_arr;
+
+[comp_ratio_test,sortIdx] = sort(comp_ratio_test,'descend');
+comp_ratio_train = comp_ratio_train(sortIdx);
+
+% sort B using the sorting index
+labels = industry_codes.Description(sortIdx);
+% remove nans
+nanIdx = ~isnan(comp_ratio_test) & ~isnan(comp_ratio_train);
+comp_ratio_test = comp_ratio_test(nanIdx);
+comp_ratio_train = comp_ratio_train(nanIdx);
+% combine the ratio in a matrix
+comp_ratio = [comp_ratio_test, comp_ratio_train];
 labels = labels(nanIdx);
-bar(comp_ratio)
-set(gca,'xticklabel',labels,'fontsize',12)
-xtickangle(30)
+
+fig = figure();
+c_map = lines(3);
+hold on
+b1 = bar(comp_ratio_test,'stack');
+b1.FaceColor = c_map(1,:);
+b1.EdgeAlpha = 0;
+b2 = bar(-comp_ratio_train,'stack');
+b2.FaceColor = c_map(3,:);
+b2.EdgeAlpha = 0;
+hold off
+
 ylabel('RMSE competitive ratio', 'fontsize', 14)
-yline(1,'-k','', 'Color', 'r', 'LineWidth', 2)
+yline(1,'-k','', 'Color', c_map(2,:), 'LineWidth', 2)
+yline(-1,'-k','', 'Color', c_map(2,:), 'LineWidth', 2)
+
+set(gca,'yticklabel',num2str(abs(get(gca,'ytick').')))
+
+lgd = legend('Test ratio', 'Train ratio', 'Ratio = 1');
+set(lgd, 'Location', 'Best')
+%set(gca,'xticklabel',labels,'fontsize',9)
+%xtickangle(50)
+
+name = "comp_ratio_test_train";
+fig_name = name + '.fig';
+png_name = name + '.png';
+savefig(fig_name);
+saveas(fig,png_name,'png');
+
+%% Plot just the test:
+fig = figure();
+b = bar(comp_ratio_test);
+b.FaceColor = c_map(1,:);
+b.EdgeAlpha = 0;
+
+
+ylabel('RMSE competitive ratio', 'fontsize', 14)
+yline(1,'-k','', 'Color', c_map(2,:), 'LineWidth', 2)
+n_winners = sum(comp_ratio_test > 1);
+xline(n_winners+0.5,'-','.', 'Color', c_map(3,:), 'LineWidth', 1)
+
+lgd = legend('Test ratio', 'Ratio = 1');
+set(lgd, 'Location', 'Best')
+
+midpoint = 0.719;
+annotation('textarrow',[.3,midpoint],...
+           [.5,.5],'String','50 Industries ', 'LineWidth', 1)
+annotation('textarrow',[0.78,midpoint,],...
+           [.5,.5],'String','15 Industries', 'LineWidth', 1)       
+
+name = "comp_ratio_test";
+fig_name = name + '.fig';
+png_name = name + '.png';
+savefig(fig_name);
+saveas(fig,png_name,'png');
+
+
+
+
+
+
+
+
 
 %% Plot 3D
 [X,Y] = meshgrid(min(features(:,1)):10000:max(features(:,1)), ...
@@ -157,3 +263,23 @@ scatter(features(:,1), response)
 hold off
 legend('Cobb-Douglas','Monotone Convex', 'Data')
 
+%% Plot contour
+
+[X,Y] = meshgrid(min(features(:,1)):10000:max(features(:,1)), ...
+                 min(features(:,2)):10000:max(features(:,2)));
+c = mean(features(:,3));
+data = [X(:), Y(:), c*ones(length(X(:)), 1)];
+Z = exp(mdl.predict(log(data)));
+Z = reshape(Z, size(X));
+c = contour(X,Y,Z);
+hold on
+% poly function
+Y_hat = zeros(length(data), 1);
+for j = 1:length(Y_hat)
+    Y_hat(j) = replace(p, x, data(j,:)/100000);
+end
+Z = 100000*Y_hat;
+Z = reshape(Z, size(X));
+c = contour(X,Y,Z);
+hold off
+legend('Cobb-Douglas','Monotone Convex')
